0029ef60cc61142fd74bcd60368e46ddc30b8a02
luisencastin <macbookair@192.168.1.10>
2023-02-10 13:39:20 -0800

    Prueba de endpoint welcome

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.get("/", (req,res) => {
   res.status(200).send({msg: "Hola tincode"});
 });
 
+app.get("/welcome", (req,res) => {
+  res.status(200).send({msg: "bienvenido"});
+});
+
 app.listen(PORT, () => {
   console.log(`Tu server está listo en el puerto ${PORT}`)
 })
