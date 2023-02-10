dcffaa09d15f8c8e5ca3ed82a2195311a3e11a12
luisencastin <macbookair@192.168.1.10>
2023-02-10 14:40:29 -0800

    Conectando servidor con mongo

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const bodyParse = require('body-parser');
+const mongoose = require("mongoose");
+const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 300;
 
-app.use(bodyParse.urlencoded({ extended:true }));
-app.use(bodyParse.json());
+app.use(bodyParse.json({ limit: '30mb', extended: true }));
+app.use(bodyParse.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
 
+// Connection URI
+const uri = 'mongodb+srv://luisencastin:Rayuela1@cluster0.whup5ue.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 3000;
+
+//Connection to database
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+//Schema
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: String,
+  email: String
+});
+
+const User = mongoose.model('User', UserSchema);
+
+//Peticiones
 app.get("/", (req,res) => {
   res.status(200).send({msg: "Hola tincode"});
 });
@@ -15,6 +37,16 @@ app.get("/welcome", (req,res) => {
   res.status(200).send({msg: "bienvenido"});
 });
 
-app.listen(PORT, () => {
-  console.log(`Tu server está listo en el puerto ${PORT}`)
-})
+app.post("/user", function (req, res) {
+  const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+  });
+  user.save(function (err) {
+      if (err) {
+          res.redirect("/error");
+      } else {
+          res.redirect("/thank-you");
+      }
+  });
+});
